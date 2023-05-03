4ffdbe24f0ebb7aff728a035d2e38657b58bd7f8
rajat-gith <rajatnayak1582002@gmail.com>
2023-05-03 13:42:39 +0530

    c

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,6 +13,7 @@ function HomeScreen() {
 
   useEffect(() => {
     dispatch(listProducts());
+    console.log(Object.entries(products));
   }, [dispatch]);
 
   return (
@@ -24,7 +25,7 @@ function HomeScreen() {
         <Message variant="danger">{error}</Message>
       ) : (
         <Row>
-          {products.map((product) => (
+          {Object.entries(products).map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
             </Col>
